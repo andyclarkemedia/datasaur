6d78ab595cee04955b3c2f5d03ec5b2aa473e4aa
Andy Clarke <andrewjhclarke@gmail.com>
2020-02-22 10:06:26 +0000

    Level 1 Update

--- src/utils.js
+++ src/utils.js
@@ -18,13 +18,52 @@ export const arraysMatch = function (arr1, arr2) {
 
 };
 
-export const levelUp = function() {
-	return 1;
+
+//==================
+// HOVER OVER IMAGE MAKE TEXT APPEAR / DISAPPEAR 
+//==================
+
+export const hoverHeadTextAppear = function(textid) {
+	const text = document.querySelector(textid)
+	text.style.display = "block"
 }
 
-export const episodeUp = function() {
-	return { };
+export const hoverHeadTextDisappear = function(textid) {
+	const text = document.querySelector(textid)
+	text.style.display = "none" 
 }
+
+//==================
+// ADD BORDER ON CLICK 
+//==================
+
+export const addBorderOnClick = function(elementId, button) {
+	const element = document.querySelector(elementId);
+	element.style.border = "2px solid black";
+	element.style.backgroundColor = "#57BA98"
+
+	const setButton = document.querySelector(button);
+	console.log(setButton.innerHTML)
+	setButton.disabled = false;
+}
+
+
+
+//==================
+// UPDATE GAME STATE WITH ORDER DETAILS  
+//==================
+
+
+export const confirmOrder = function() {
+	const cheese = document.querySelector('#cheese-order-input');
+	const tomato = document.querySelector('#tomato-order-input');
+	const basil = document.querySelector('#basil-order-input');
+
+	this.festivalFoodOrder.cheese = cheese.value;
+	this.festivalFoodOrder.tomato = tomato.value;
+	this.festivalFoodOrder.basil = basil.value;
+}
+
 
 
 
